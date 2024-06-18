20286c810fdb3a0158be27eb32d24149cd0b9906
anakin-c <tsibulko.vladimir@tftc.dev>
2024-06-19 00:56:35 +0700

    sprint_1: remove comment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import { resolve } from 'path';
 
 export default defineConfig({
-  // root: resolve(__dirname, 'static'),
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
