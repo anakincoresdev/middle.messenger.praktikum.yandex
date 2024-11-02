1c01753e8696c23e8d09b6b0c3ce7ff02a843ecb
anakin-c <tsibulko.vladimir@tftc.dev>
2024-11-02 14:42:29 +0700

    sprint_2: review fixes

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,6 +1,6 @@
-/* eslint-disable-next-line max-len */
 import { Component } from '@/core/component.ts';
 
+/* eslint-disable-next-line max-len */
 const emailRegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 type ValidationRules = Record<string, (value: string) => boolean>;
